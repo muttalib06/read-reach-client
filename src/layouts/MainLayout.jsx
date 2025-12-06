f5b1ab950ec90f81932682f096dcefab8ecd4e09
Md. Muttalib <mdabdulmottalib2222@gmail.com>
2025-12-06 06:52:47 +0600

    added basic setup and all files

---
+++ src/layouts/MainLayout.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from '../components/sharedComponents/Navbar';
+
+const MainLayout = () => {
+        return (
+                <div>
+                        <Navbar></Navbar>
+                        <h1>Main Layout</h1>
+                        
+                </div>
+        );
+};
+
+export default MainLayout;
---
+++ src/router/Router.jsx
@@ -0,0 +1,13 @@
+import { createBrowserRouter } from "react-router";
+import MainLayout from "../layouts/MainLayout";
+
+export const router = createBrowserRouter([
+        {
+                path:"/",
+                element:<MainLayout></MainLayout>,
+                children:[
+                        
+                ]
+        }
+
+])
